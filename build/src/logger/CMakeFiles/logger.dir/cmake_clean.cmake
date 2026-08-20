d3daef0c29e0ecb22d0357500bf32d43d21419ab
l-Monarch-l <ubuntu@Monarch.>
2026-08-20 18:58:45 +0300

    library

---
+++ build/src/logger/CMakeFiles/logger.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logger.dir/FileSink.cpp.o"
+  "CMakeFiles/logger.dir/FileSink.cpp.o.d"
+  "CMakeFiles/logger.dir/Logger.cpp.o"
+  "CMakeFiles/logger.dir/Logger.cpp.o.d"
+  "CMakeFiles/logger.dir/SocketSink.cpp.o"
+  "CMakeFiles/logger.dir/SocketSink.cpp.o.d"
+  "liblogger.a"
+  "liblogger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/tests/CMakeFiles/logger_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logger_tests.dir/test_logger.cpp.o"
+  "CMakeFiles/logger_tests.dir/test_logger.cpp.o.d"
+  "logger_tests"
+  "logger_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logger_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
